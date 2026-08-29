d321133c2960db975d6d48b0c4dd4489381b9837
Markus300i <patryk.ruszkiewicz2@gmail.com>
2026-08-29 14:28:59 +0200

    Export OpenCut adapter and task engine APIs

--- csp_studio/__init__.py
+++ csp_studio/__init__.py
@@ -2,9 +2,12 @@
 
 from .asset_manager import AssetManager
 from .models import Asset, Project, Scene, SceneRevision, ShotPlan
+from .opencut_adapter import build_manifest as build_opencut_manifest
+from .opencut_adapter import export_manifest as export_opencut_manifest
 from .scene_ops import SceneOperations
 from .shot_director import ShotAudit, ShotDirector
 from .store import StudioStore
+from .task_engine import StudioTask, TaskEngine
 
 __all__ = [
     "Asset",
@@ -17,4 +20,8 @@ __all__ = [
     "ShotDirector",
     "ShotAudit",
     "StudioStore",
+    "StudioTask",
+    "TaskEngine",
+    "build_opencut_manifest",
+    "export_opencut_manifest",
 ]
