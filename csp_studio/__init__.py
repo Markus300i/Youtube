6cf33614c226a1389c90a25169523aa7b9cc8d37
Markus300i <patryk.ruszkiewicz2@gmail.com>
2026-08-29 11:03:17 +0200

    chore(studio): export asset manager types

--- csp_studio/__init__.py
+++ csp_studio/__init__.py
@@ -1,10 +1,13 @@
 """CSP Studio domain layer built on top of the existing CSP Automation renderer."""
 
-from .models import Project, Scene, SceneRevision, ShotPlan
-from .shot_director import ShotDirector, ShotAudit
+from .asset_manager import AssetManager
+from .models import Asset, Project, Scene, SceneRevision, ShotPlan
+from .shot_director import ShotAudit, ShotDirector
 from .store import StudioStore
 
 __all__ = [
+    "Asset",
+    "AssetManager",
     "Project",
     "Scene",
     "SceneRevision",
