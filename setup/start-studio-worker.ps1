3cb7f06d8ddc043a4e383c0d94319f787ad76435
Markus300i <patryk.ruszkiewicz2@gmail.com>
2026-08-30 23:20:40 +0200

    Start Studio Worker with watchdog

--- setup/start-studio-worker.ps1
+++ setup/start-studio-worker.ps1
@@ -1,14 +1,20 @@
 param(
-    [string]$TaskName = "CSP Studio Worker"
+    [string]$TaskName = "CSP Studio Worker",
+    [string]$WatchdogTaskName = "CSP Studio Worker Watchdog"
 )
 
 $ErrorActionPreference = "Stop"
 
 $task = Get-ScheduledTask -TaskName $TaskName -ErrorAction Stop
+$watchdog = Get-ScheduledTask -TaskName $WatchdogTaskName -ErrorAction Stop
+
+Enable-ScheduledTask -TaskName $WatchdogTaskName | Out-Null
+Enable-ScheduledTask -TaskName $TaskName | Out-Null
 Start-ScheduledTask -InputObject $task
 Start-Sleep -Milliseconds 500
-$info = Get-ScheduledTaskInfo -InputObject $task
+$info = Get-ScheduledTaskInfo -TaskName $TaskName
 
 Write-Host "Started scheduled task: $TaskName"
+Write-Host "Watchdog enabled: $WatchdogTaskName"
 Write-Host "State: $((Get-ScheduledTask -TaskName $TaskName).State)"
 Write-Host "LastTaskResult: $($info.LastTaskResult)"
